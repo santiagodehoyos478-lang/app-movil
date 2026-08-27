8dfff165fd405605dbfdbe62bc625978e8b184ce
NikolO <tatisnik1022@gmail.com>
2026-08-27 12:51:32 -0500

    Documentacionç

--- android/app/src/main/kotlin/com/example/serviat_movil/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.serviat_movil
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/constants/app_constants.dart
+++
@@ -1,8 +0,0 @@
-class AppConstants {
-  static const String appName = 'AR SERVICIO';
-  static const String userRole = 'admin';
-
-  // Para dispositivo físico, usa la IP de tu PC (ej: 192.168.0.15)
-  // Para Emulador, usa 10.0.2.2
-  static const String apiBaseUrl = 'http://192.168.0.15:3001/api';
-}
