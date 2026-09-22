c8b4ef27516ce0444fabd1519f95a1548d4caae0
Danna586 <cb500889@gmail.com>
2026-09-22 06:41:07 -0500

    Credenciales del proyecto móvil con Supabase

---
+++ constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  // credenciales
+  static const String supabaseUrl = 'https://nlhettasjigvsazbuqly.supabase.co';
+  static const String supabaseAnonKey = 'sb_publishable_TpOo6QlroBnYmXXC3qDCBA_fllserVt';
+}
